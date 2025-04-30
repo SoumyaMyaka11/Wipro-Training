43b05d737e9df55b78b9938b84dec22ba8d42286
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-04-30 09:07:17 +0530

    gradeprogram

---
+++ April-30-2025/Grade.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Grade
+{
+    public static void Main(string[] args)
+    {
+       int marks =78;
+       if(marks>=90)
+       {
+            Console.WriteLine("Grade A");
+       }
+       else if(marks>=75)
+       {
+            Console.WriteLine("Grade B");
+       }
+       else if(marks>=60)
+       {
+            Console.WriteLine("Grade C");
+       }
+        else 
+        {
+            Console.WriteLine("Grade D");
+        }
+    }
+}
