2caf9ae967ddcb6c715af7f0e3f4aa7cf995f034
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-04-30 10:18:58 +0530

    squares

---
+++ April-30-2025/square.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Grade
+{
+    public static void Main(string[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            Console.WriteLine("square of "+i+" is "+i*i);
+        }
+    }
+}
