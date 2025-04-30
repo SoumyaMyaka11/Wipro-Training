9e68094f9107f12d555180da5a1cf80ada364d38
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-04-30 09:59:03 +0530

    nestedforloop

---
+++ April-30-2025/Nestedfor.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Grade
+{
+    public static void Main(string[] args)
+    {
+       for(int r=1; r<=3; r++)
+       {
+           for(int c=1; c<=3; c++)
+           {
+               Console.Write(c+" ");
+           }
+           Console.WriteLine();
+       }
+                
+    }
+}
