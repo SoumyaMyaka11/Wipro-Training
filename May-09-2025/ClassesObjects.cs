c11cd24641791c7a6e0133d8336fdf8e97af634e
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-05-09 09:42:11 +0530

    classesandobjects

---
+++ May-09-2025/ClassesObjects.cs
@@ -0,0 +1,30 @@
+using System;
+
+ class Student
+{
+    public int RollNumber;
+    public string Name;
+    public int Marks;
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Rollnumber : {RollNumber}");
+        Console.WriteLine($"RollNumber : {Name}");
+        Console.WriteLine($"Marks : {Marks}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //instantiation
+        Student student1 = new Student();
+
+        student1.RollNumber = 1;
+        student1.Name = "Swetha";
+        student1.Marks = 88;
+
+        student1.PrintDetails();
+    }
+}
