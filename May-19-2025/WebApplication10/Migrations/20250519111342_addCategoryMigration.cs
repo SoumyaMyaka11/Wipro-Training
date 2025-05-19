a80bfc9aaac4e036496e0333d40ab23f281fec1d
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-05-19 17:31:31 +0530

    webapplication

---
+++ May-19-2025/WebApplication10/Migrations/20250519111342_addCategoryMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication10.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategoryMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryType",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryType",
+                table: "Category");
+        }
+    }
+}
---
+++ May-19-2025/WebApplication10/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication10.Models
+{
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryType { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
