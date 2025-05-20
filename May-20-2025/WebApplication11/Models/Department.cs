fe50ff3826f075039029675219e5acacba50b130
SoumyaMyaka <soumyareddy1008@gmail.com>
2025-05-20 15:27:49 +0530

    webapplication11

---
+++ May-20-2025/WebApplication11/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication11.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+
+    }
+}
---
+++ May-20-2025/WebApplication11/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication11.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+    }
+}
